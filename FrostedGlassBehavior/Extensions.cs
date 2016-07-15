d4f052faf866ab1b566d19a096415c7fe949c80a
Lance Contreras <lancecontreras@gmail.com>
2016-07-15 15:48:19 -0400

    Initial commit.

---
+++ FrostedGlassBehavior/Extensions.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace FrostedGlassBehavior
+{
+  public static class Extensions
+  {
+    public static Point GetCoordinates(this FrameworkElement element, UIElement parent = null, bool zero = false)
+    {
+      if (parent == null)
+        parent = Window.Current.Content;
+
+      var transform = element.TransformToVisual(parent);
+      Point point;
+      if (zero)
+        point = transform.TransformPoint(new Point(0, 0));
+      else
+        point = transform.TransformPoint(new Point(element.ActualWidth / 2, element.ActualHeight / 2));
+      return point;
+    }
+
+    public static async Task<IRandomAccessStream> RenderToRandomAccessStreamAsync(this UIElement element)
+    {
+      if (element == null) throw new NullReferenceException();
+
+      RenderTargetBitmap rtb = new RenderTargetBitmap();
+      await rtb.RenderAsync(element);
+
+      var pixelBuffer = await rtb.GetPixelsAsync();
+      var pixels = pixelBuffer.ToArray();
+
+      var displayInformation = DisplayInformation.GetForCurrentView();
+
+      var stream = new InMemoryRandomAccessStream();
+      var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, stream);
+      encoder.SetPixelData(BitmapPixelFormat.Bgra8,
+                           BitmapAlphaMode.Premultiplied,
+                           (uint)rtb.PixelWidth,
+                           (uint)rtb.PixelHeight,
+                           displayInformation.RawDpiX,
+                           displayInformation.RawDpiY,
+                           pixels);
+
+      await encoder.FlushAsync();
+      stream.Seek(0);
+
+      return stream;
+    }
+
+    public static async Task<BitmapImage> GetBlurredBackground(this UIElement element, float blurrAmount = 5.0f)
+    {
+      using (var stream = await element.RenderToRandomAccessStreamAsync())
+      {
+        var device = new CanvasDevice();
+        var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+
+        var renderer = new CanvasRenderTarget(device,
+                                              bitmap.SizeInPixels.Width,
+                                              bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+        using (var ds = renderer.CreateDrawingSession())
+        {
+          var blur = new GaussianBlurEffect();
+          blur.BlurAmount = blurrAmount;
+          blur.Source = bitmap;
+          ds.DrawImage(blur);
+        }
+
+        stream.Seek(0);
+        await renderer.SaveAsync(stream, CanvasBitmapFileFormat.Png);
+
+        BitmapImage image = new BitmapImage();
+        image.SetSource(stream);
+        return image;
+      }
+    }
+
+    public static async Task<BitmapImage> GetBlurredImageAsync(this IRandomAccessStream stream)
+    {
+      var device = new CanvasDevice();
+
+      var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+
+      var renderer = new CanvasRenderTarget(device,
+                                            bitmap.SizeInPixels.Width,
+                                            bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+      using (var ds = renderer.CreateDrawingSession())
+      {
+        var blur = new GaussianBlurEffect();
+        blur.BlurAmount = 10.0f;
+        blur.Source = bitmap;
+        ds.DrawImage(blur);
+      }
+
+      stream.Seek(0);
+      await renderer.SaveAsync(stream, CanvasBitmapFileFormat.Png);
+
+      BitmapImage image = new BitmapImage();
+      image.SetSource(stream);
+      return image;
+    }
+    
+  }
+}
---
+++ FrostedGlassBehavior/FrostedGlassBehavior.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace FrostedGlassBehavior
+{
+  public class FrostedGlassBehavior : Behavior<FrameworkElement>
+  {
+    static BitmapImage _blurredBackground = null;
+    static int _tickCtr = 0;
+    static bool _isBlurred = false;
+
+    Frame _layoutRoot;
+    DispatcherTimer _timer = new DispatcherTimer();
+    FrameworkElement _panel;
+
+    protected override void OnAttached()
+    {
+      _panel = this.AssociatedObject as FrameworkElement;
+      _layoutRoot = (Window.Current.Content as Frame);
+      _panel.Loaded += Panel_Loaded;
+    }
+
+    private void Layoutroot_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+      _layoutRoot.SizeChanged -= Layoutroot_SizeChanged;
+      Refresh();
+    }
+
+    private void _layoutRoot_Navigated(object sender, Windows.UI.Xaml.Navigation.NavigationEventArgs e)
+    {
+      _layoutRoot.Navigated -= _layoutRoot_Navigated;
+      Refresh();
+    }
+
+
+    void Refresh()
+    {
+      _layoutRoot.SizeChanged -= Layoutroot_SizeChanged;
+      _blurredBackground = null;
+      _tickCtr = 0;
+      _isBlurred = false;
+      _timer.Start();
+    }
+
+
+    private async void _timer_Tick(object sender, object e)
+    {
+      if (_panel.Visibility == Visibility.Visible)
+        _panel.Visibility = Visibility.Collapsed;
+      if (_isBlurred == false && _panel.Visibility == Visibility.Collapsed)
+      {
+        if (_blurredBackground == null)
+        {
+          _blurredBackground = await _layoutRoot.GetBlurredBackground(8.0f);
+          _isBlurred = true;
+        }
+      }
+      if (_isBlurred)
+      {
+        Point panelPoint = _panel.GetCoordinates(_layoutRoot, false);
+        Point layoutRootPoint = _layoutRoot.GetCoordinates(_layoutRoot, false);
+
+        var distance = GetDistance(panelPoint, layoutRootPoint);
+        Transform moveTransform = new TranslateTransform() { X = distance.X, Y = distance.Y };
+        Brush backgroundBrush = new ImageBrush() { ImageSource = _blurredBackground, Stretch = Stretch.None, Transform = moveTransform, AlignmentX = AlignmentX.Center, AlignmentY = AlignmentY.Center };
+
+        if (_panel is Border)
+          (_panel as Border).Background = backgroundBrush;
+        if (_panel is Panel)
+          (_panel as Panel).Background = backgroundBrush;
+
+        _panel.Visibility = Visibility.Visible;
+        (sender as DispatcherTimer).Stop();
+        _layoutRoot.SizeChanged += Layoutroot_SizeChanged;
+        _layoutRoot.Navigated += _layoutRoot_Navigated; ;
+      }
+      _tickCtr++;
+    }
+
+
+    private void Panel_Loaded(object sender, RoutedEventArgs e)
+    {
+      _panel.Loaded -= Panel_Loaded;
+      _timer.Interval = new TimeSpan(10);
+      _timer.Tick += _timer_Tick;
+      _timer.Start();
+    }
+
+    protected override void OnDetaching()
+    {
+      //base.OnDetaching();
+      _timer.Stop();
+    }
+    private Point GetDistance(Point p, Point p2)
+    {
+      return new Point(((-1 * p2.X) + p.X) * -1, ((-1 * p2.Y) + p.Y) * -1);
+    }
+  }
+}
